1c9f0c17e195d3fcc1cf26cf7c0a5d1b595fe585
waszak <kwaszak67@gmail.com>
2015-01-16 19:41:56 +0100

    Generowanie typów

--- main.cpp
+++ main.cpp
@@ -13,6 +13,12 @@ void generate_types(Config &types, int ntypes =17, int params =9){
     types.clear();
     if(ntypes == -1){
         types.push_back({1.0,1.0,1.0});
+    }else{
+        in_range(i, 0, ntypes){
+            vector <float> a = vector <float>();
+            in_range(j, 0, params) a.push_back(rand()%5);
+            types.push_back(a);
+        }
     }
 }
 void generate_data( int n, vector<int > &data, int ntypes=17){
@@ -24,6 +30,7 @@ void generate_data( int n, vector<int > &data, int ntypes=17){
                 data.push_back(1);
             }
         }
+        //...
     }else{
         in_range(j,0,n){
             data.push_back( rand()%ntypes);
@@ -33,9 +40,10 @@ void generate_data( int n, vector<int > &data, int ntypes=17){
 
 int main(){
     srand( time( NULL ) );
+    int ntypes= 5/*-1*/, params = 3;
 
     Config types = Config();
-    generate_types(types, -1);
+    generate_types(types, ntypes, params);
     cout<<"Types"<<endl;
     for(auto & i : types ){
         for(float j : i){
@@ -46,7 +54,7 @@ int main(){
 
     cout<<"Generate Id to type"<<endl;
     vector<int> x = vector<int>();
-    generate_data(10, x);
+    generate_data(10, x, ntypes);
     for(int i : x ){
         cout<<i<<endl;
     }
