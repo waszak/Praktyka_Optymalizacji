69b618a289b257502fe6318cef186d7ad673801e
waszak <kwaszak67@gmail.com>
2015-01-16 19:14:46 +0100

    Split Types and Data.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
 #define in_range(i, a,b)  for(int i =(a); i<(b); i++)
+
 using namespace std;
+typedef  vector<vector<float>> Config;
 
 //void generate_types(int types, int params)
-void generate_data( int n, vector<vector<float>> &data, int types=17, int params=9){
+void generate_types(Config &types, int ntypes =17, int params =9){
+    types.clear();
+    if(ntypes == -1){
+        types.push_back({1.0,1.0,1.0});
+    }
+}
+void generate_data( int n, vector<int > &data, int ntypes=17){
     data.clear();
     if( n ==-1){
         // Jakieœ testy poprawnoœciowe
-        if(types ==1 ){
+        if(ntypes ==1 ){
             in_range(j,0,10){
-                data.push_back({1.0,1.0,1.0});
+                data.push_back(1);
             }
         }
     }else{
         in_range(j,0,n){
-            cout<<j;
+            data.push_back( rand()%ntypes);
         }
     }
-
 }
-int main(){
-    vector<vector<float>> x = std::vector<vector<float>>();
-    generate_data(-1, x, 1);
 
-    for(vector<float> & i : x ){
+int main(){
+    srand( time( NULL ) );
+
+    Config types = Config();
+    generate_types(types, -1);
+    cout<<"Types"<<endl;
+    for(auto & i : types ){
         for(float j : i){
             cout<<j<<", ";
         }
         cout <<endl;
+    }
+
+    cout<<"Generate Id to type"<<endl;
+    vector<int> x = vector<int>();
+    generate_data(10, x);
+    for(int i : x ){
+        cout<<i<<endl;
     }
     return 0;
 }
