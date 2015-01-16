e705dd09d1722ca663ace528dee4c4c418dbbfa4
waszak <kwaszak67@gmail.com>
2015-01-16 19:55:54 +0100

    Cost function - Basic Draft

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@
 #define in_range(i, a,b)  for(int i =(a); i<(b); i++)
 
 using namespace std;
-typedef  vector<vector<float>> Config;
+typedef  vector<vector<int>> Config;
 
 //void generate_types(int types, int params)
 void generate_types(Config &types, int ntypes =17, int params =9){
     types.clear();
     if(ntypes == -1){
-        types.push_back({1.0,1.0,1.0});
+        types.push_back({1,1,1});
     }else{
         in_range(i, 0, ntypes){
-            vector <float> a = vector <float>();
+            vector <int> a = vector <int>();
             in_range(j, 0, params) a.push_back(rand()%5);
             types.push_back(a);
         }
@@ -38,6 +38,15 @@ void generate_data( int n, vector<int > &data, int ntypes=17){
     }
 }
 
+int compute_cost(vector<int> &data, Config &types, int params){
+    int total_time = 0;
+    vector<int> assembly_line = vector<int>();
+    while( assembly_line.size() > 0){
+
+    }
+    return total_time;
+}
+
 int main(){
     srand( time( NULL ) );
     int ntypes= 5/*-1*/, params = 3;
@@ -46,7 +55,7 @@ int main(){
     generate_types(types, ntypes, params);
     cout<<"Types"<<endl;
     for(auto & i : types ){
-        for(float j : i){
+        for(int j : i){
             cout<<j<<", ";
         }
         cout <<endl;
@@ -58,5 +67,7 @@ int main(){
     for(int i : x ){
         cout<<i<<endl;
     }
+
+    cout<<"Cost of permutation\n"<< compute_cost(x, types, params)<<endl;
     return 0;
 }
