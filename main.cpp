2582bf45fe9094236ba41037e2af0e51b1f1d055
waszak <kwaszak67@gmail.com>
2015-01-17 01:49:15 +0100

    Now We have simulation of Assembly line(without workers)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
-#define in_range(i, a,b)  for(int i =(a); i<(b); i++)
+#define in_range(i, a,b )  for(int i =(a); i < (b); i++)
 
 using namespace std;
 typedef  vector<vector<int>> Config;
@@ -43,7 +43,7 @@ void generate_data( int n, vector<int > &data, int ntypes=17){
 int compute_cost(vector<int> & data, Config &types, int params){
     int total_time = 0, elements_on_line = 1, idx =0;
     auto comp = [] (tuple<int,int> &a, tuple<int,int> &b) -> bool { return get<1>(a) < get<1>(b); };
-    std::priority_queue<tuple<int,int>,std::vector<tuple<int,int>>, decltype(comp) > pq (comp);
+    priority_queue<tuple<int,int>,std::vector<tuple<int,int>>, decltype(comp) > pq (comp);
 
     vector<int> assembly_line = vector<int>();
     assembly_line.resize(params, -1);
@@ -53,40 +53,58 @@ int compute_cost(vector<int> & data, Config &types, int params){
 
     while( elements_on_line > 0){
         //Print how out line looks atm
-        cout<<"-------("<<idx<<", "<<pq.size()<<")\n";
+        cin.get();
+        cout<<"-------("<<idx<<", "<<elements_on_line<<")\n";
         for(int i: assembly_line){
             cout<<i<<endl;
         }
         cout<<"-------\n";
-        //We are looking for first element who can move
+        //We are looking for time of first element who can move
         //because other platform can be blocked.
         tuple<int, int> a = make_tuple(-1, -1);
-        vector<tuple<int,int>> removed_el = vector<tuple<int,int>>();
         while(!pq.empty()){
             a = pq.top();
             pq.pop();
             if(get<0>(a) + 1 == params || assembly_line[get<0>(a) + 1 ] == -1){
-                for( auto i: removed_el){
-                    pq.push(i);
+                while(!pq.empty()){
+                    pq.pop(); // faster would be replacing with new queue.
                 }
                 break;
             }
-            removed_el.push_back(a);
         }
-
+        cout<<get<0>(a)<<" "<<get<1>(a)<<endl;
         total_time += get<1>(a);
-        if(get<0>(a) + 1 == params){
-            elements_on_line--;
-        }else{
-            assembly_line[get<0>(a) + 1] = assembly_line[get<0>(a)];
-            pq.push(make_tuple(get<0>(a)+1,types[data[assembly_line[get<0>(a)]]][get<0>(a)+1]));
-        }
-        assembly_line[get<0>(a)]  = -1;
 
-        // we move elements
-        /*
-        * TODO
+        // we move elements. We need to check all elements from Right to Left.
+        if(assembly_line[params-1] != -1 && types[data[assembly_line[params-1]]][params-1] <= get<1>(a)){
+            elements_on_line--;
+            assembly_line[params - 1 ] = -1;
+        }
+        in_range(i,1, params){
+            if(assembly_line[params-i-1] == -1 ){
+                continue;
+            }
+            if(types[data[assembly_line[params-i-1]]][params-i-1] > get<1>(a)){
+                pq.push(make_tuple(params-i-1, types[data[assembly_line[params-i-1]]][params-i-1]));
+                continue;
+            }
+            if(assembly_line[params-i ] == -1) {
+                assembly_line[params-i] = assembly_line[params-i-1 ];
+                pq.push(make_tuple(params-i ,types[data[assembly_line[params-i]]][params-i]));
+                assembly_line[params-i-1 ]  = -1;
+            }else{
+                pq.push(make_tuple(params-i-1, types[data[assembly_line[params-i-1]]][params-i-1]));
+            }
+
+        }/*
+        cout<<"****\n";
+        in_range(i,0, params){
+            cout<<assembly_line[i]<<endl;
+        }
+        cout<<"****\n";
         */
+
+        //add new element if possible
         if(assembly_line[0] == -1 && idx < data.size() ){
             assembly_line[0] = data[idx];
             pq.push(make_tuple(0,types[data[idx++]][0]));
