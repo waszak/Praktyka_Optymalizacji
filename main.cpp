b2cb89d1baedac4707f8d3b32f8fea77926e534d
waszak <kwaszak67@gmail.com>
2015-01-17 11:57:25 +0100

    Update times

--- main.cpp
+++ main.cpp
@@ -46,18 +46,21 @@ int compute_cost(vector<int> & data, Config &types, int params){
     priority_queue<tuple<int,int>,std::vector<tuple<int,int>>, decltype(comp) > pq (comp);
 
     vector<int> assembly_line = vector<int>();
+    vector<int> assembly_times_line = vector<int>();
     assembly_line.resize(params, -1);
+    assembly_times_line.resize(params,0);
 
-    assembly_line[0] = data[idx];
-    pq.push(make_tuple(0,types[data[idx++]][0]));
+    assembly_line[0] = data[idx++];
+    assembly_times_line[0] = types[assembly_line[0]][0];
+    pq.push(make_tuple(0,assembly_times_line[0]));
 
     while( elements_on_line > 0){
         //Print how out line looks atm
-        //cin.get();
+        cin.get();
         cout<<"-------("<<idx<<", "<<elements_on_line<<")\n";
         int j = 0;
         for(int i: assembly_line){
-            cout<<((i == -1)?-1:types[i][j++])<<" "<< i<<endl;
+            cout<<((i == -1)?-1:assembly_times_line[j++])<<" "<< i<<endl;
         }
         cout<<"-------\n";
         //We are looking for time of first element who can move
@@ -73,28 +76,33 @@ int compute_cost(vector<int> & data, Config &types, int params){
                 break;
             }
         }
-        cout<<get<0>(a)<<" "<<get<1>(a)<<endl;
+        cout<<get<1>(a)<<" "<<assembly_line[get<0>(a)]<<endl;
         total_time += get<1>(a);
 
         // we move elements. We need to check all elements from Right to Left.
-        if(assembly_line[params-1] != -1 && types[data[assembly_line[params-1]]][params-1] <= get<1>(a)){
+        if(assembly_line[params-1] != -1 && assembly_times_line[params-1] <= get<1>(a)){
             elements_on_line--;
             assembly_line[params - 1 ] = -1;
+            assembly_times_line[params - 1] = 0;
         }
         in_range(i,1, params){
             if(assembly_line[params-i-1] == -1 ){
                 continue;
             }
-            if(types[data[assembly_line[params-i-1]]][params-i-1] > get<1>(a)){
-                pq.push(make_tuple(params-i-1, types[data[assembly_line[params-i-1]]][params-i-1]));
+            if(assembly_times_line[params-i-1] > get<1>(a)){
+                assembly_times_line[params-i-1] =- get<1>(a);
+                pq.push(make_tuple(params-i-1, assembly_times_line[params-i-1]));
                 continue;
             }
             if(assembly_line[params-i ] == -1) {
-                assembly_line[params-i] = assembly_line[params-i-1 ];
-                pq.push(make_tuple(params-i ,types[data[assembly_line[params-i]]][params-i]));
+                assembly_line[params - i] = assembly_line[params-i-1 ];
+                assembly_times_line[params -i] = types[assembly_line[params-i]][params-i];
+                pq.push(make_tuple(params-i, assembly_times_line[params -i] ));
                 assembly_line[params-i-1 ]  = -1;
+                assembly_times_line[params -i-1] = 0;
             }else{
-                pq.push(make_tuple(params-i-1, types[data[assembly_line[params-i-1]]][params-i-1]));
+                assembly_times_line[params-i-1] =- get<1>(a);
+                pq.push(make_tuple(params-i-1, assembly_times_line[params-i-1]));
             }
 
         }/*
@@ -108,7 +116,8 @@ int compute_cost(vector<int> & data, Config &types, int params){
         //add new element if possible
         if(assembly_line[0] == -1 && idx < data.size() ){
             assembly_line[0] = data[idx];
-            pq.push(make_tuple(0,types[data[idx++]][0]));
+            assembly_times_line[0] = types[data[idx++]][0];
+            pq.push(make_tuple(0,assembly_times_line[0]));
             elements_on_line++;
         }
     }
